42ebe68f33bd765d57d3cc46b3b133a672302e39
saikiran243 <saikiran.sd@gmail.com>
2026-06-16 19:42:12 -0400

    fix: add database_subnets to VPC module to fix RDS deployment

--- main.tf
+++ main.tf
@@ -33,6 +33,7 @@ module "vpc" {
   azs             = ["us-east-1a", "us-east-1b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  database_subnets = ["10.0.201.0/24", "10.0.202.0/24"]
 
   # COST SAVING: Single NAT Gateway instead of one per AZ
   enable_nat_gateway     = true
